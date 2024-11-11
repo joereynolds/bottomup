4c0cce6f1d36954dc56df4bdd64d2254005dd194
Joe Reynolds <joereynolds952@gmail.com>
2024-11-11 17:38:17 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='bottomup',
+    version='0.1.0',    
+    description='Most of the engine for Mr Figs',
+    url='https://github.com/joereynolds/bottomup',
+    author='Joe Reynolds',
+    packages=['src'],
+    # install_requires=[''],
+
+    classifiers=[
+        'Game Development',
+    ],
+)
